4ed08243b9387f831aab67cf7c2d7c24914b1a53
Jason Miller <jason@redconfetti.com>
2013-11-23 13:13:39 -0800

    Added method for obtaining UTC date/time string for use in filename

--- lib/annotate_gemfile/parser.rb
+++ lib/annotate_gemfile/parser.rb
@@ -5,10 +5,14 @@ module AnnotateGemfile
 
     attr_reader :gemfile_source
 
+    def self.datetime_string
+      Time.now.strftime("%m-%d-%y-%H_%M_%S")
+    end
+
     def initialize(gemfile_path)
       load_gemfile(gemfile_path)
     end
-    
+
     def load_gemfile(filepath)
       @gemfile_source = IO.read(filepath)
     end
--- spec/annotate_gemfile/parser_spec.rb
+++ spec/annotate_gemfile/parser_spec.rb
@@ -7,4 +7,12 @@ describe AnnotateGemfile::Parser do
   its(:gemfile_source)   { should be_an_instance_of String }
   
   before(:each)   { IO.should_receive(:read).and_return("File Contents") }
+
+  describe "#datetime_string" do
+    it "returns UTC date/time string formatted for use in filename" do
+      fake_current_time = Time.at(1385232600).utc
+      Time.should_receive(:now).and_return(fake_current_time)
+      expect(subject.class.datetime_string).to eq "11-23-13-18_50_00"
+    end
+  end
 end
